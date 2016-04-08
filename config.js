c94b8f3a99c4deb8a9c234540c926eb7b0ac784f
jcortes <jacortesmahmud@gmail.com>
2016-04-07 21:37:48 -0500

    On branch 01-separating-logic-from-effects modified:   config.js modified:   package.json modified:   src/index.html new file:   src/main.js deleted:    startup.js

--- config.js
+++ config.js
@@ -14,11 +14,14 @@ System.config({
   },
 
   map: {
+    "@cycle/core": "npm:@cycle/core@6.0.3",
+    "@cycle/dom": "npm:@cycle/dom@9.2.2",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
     "css": "github:systemjs/plugin-css@0.1.20",
     "json": "github:systemjs/plugin-json@0.1.0",
+    "rx": "npm:rx@4.1.0",
     "text": "github:systemjs/plugin-text@0.0.7",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -32,6 +35,17 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
+    "npm:@cycle/core@6.0.3": {
+      "rx": "npm:rx@4.1.0"
+    },
+    "npm:@cycle/dom@9.2.2": {
+      "hyperscript-helpers": "npm:hyperscript-helpers@2.0.3",
+      "matches-selector": "npm:matches-selector@1.0.0",
+      "vdom-parser": "npm:vdom-parser@1.2.1",
+      "vdom-to-html": "npm:vdom-to-html@2.2.0",
+      "virtual-dom": "npm:virtual-dom@2.1.1",
+      "x-is-array": "npm:x-is-array@0.1.0"
+    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
@@ -44,8 +58,26 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
+    "npm:error@4.4.0": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "camelize": "npm:camelize@1.0.0",
+      "string-template": "npm:string-template@0.2.1",
+      "xtend": "npm:xtend@4.0.1"
+    },
+    "npm:ev-store@7.0.0": {
+      "individual": "npm:individual@3.0.0"
+    },
+    "npm:global@4.3.0": {
+      "process": "npm:process@0.5.2"
+    },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:next-tick@0.2.2": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:param-case@1.1.2": {
+      "sentence-case": "npm:sentence-case@1.1.3"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
@@ -53,9 +85,34 @@ System.config({
     "npm:process@0.11.2": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
     },
+    "npm:rx@4.1.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:sentence-case@1.1.3": {
+      "lower-case": "npm:lower-case@1.1.3"
+    },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:vdom-parser@1.2.1": {
+      "virtual-dom": "npm:virtual-dom@2.1.1"
+    },
+    "npm:vdom-to-html@2.2.0": {
+      "escape-html": "npm:escape-html@1.0.3",
+      "param-case": "npm:param-case@1.1.2",
+      "virtual-dom": "npm:virtual-dom@2.1.1",
+      "xtend": "npm:xtend@4.0.1"
+    },
+    "npm:virtual-dom@2.1.1": {
+      "browser-split": "npm:browser-split@0.0.1",
+      "error": "npm:error@4.4.0",
+      "ev-store": "npm:ev-store@7.0.0",
+      "global": "npm:global@4.3.0",
+      "is-object": "npm:is-object@1.0.1",
+      "next-tick": "npm:next-tick@0.2.2",
+      "x-is-array": "npm:x-is-array@0.1.0",
+      "x-is-string": "npm:x-is-string@0.1.0"
     }
   }
 });
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import {Rx} from 'rx';
+
+Rx.Observable.timer(0, 1000)
+  .map((i) => `Seconds elapsed ${i}`)
+  .subscribe(text => {
+    const container = document.querySelector("#app");
+    container.textContent = text;
+  });
