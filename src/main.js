846ba2cbaa4d5c3b1d10361531d13a729f0806cd
jcortes <jacortesmahmud@gmail.com>
2016-05-01 16:07:05 -0500

    On branch 05-rxjs-read-effects-from-the-dom-click-events modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 import {Rx} from 'rx';
 
 // Logic (functional)
-function main () {
+function main (DOMSource) {
+  const click$ = DOMSource;
   return {
-    DOM: Rx.Observable.timer(0, 1000)
-      .map((i) => `Seconds elapsed ${i}`),
+    DOM: click$
+      .startWith(undefined)
+      .flatMapLatest(() =>
+        Rx.Observable.timer(0, 1000)
+        .map((i) => `Seconds elapsed ${i}`)
+      ),
     Log: Rx.Observable.timer(0, 2000)
       .map(i => i * 2)
   }
 }
+
+// source: input (read) effects
+// sink: output (write) effects
 
 // Effects (imperative)
 function DOMDriver (text$) {
@@ -16,17 +24,28 @@ function DOMDriver (text$) {
     const container = document.querySelector("#app");
     container.textContent = text;
   });
+
+  const DOMSource = Rx.Observable.fromEvent(document, 'click');
+  return DOMSource;
 }
 
 function consoleLogDriver (msg$) {
   msg$.subscribe(msg => console.log(msg));
 }
 
+// bProxy = ...
+// a = f(bProxy)
+// b = g(a)
+// bProxy.imitate(b)
+
 function run (mainFn, drivers) {
-  const sinks = mainFn();
-  Object.keys(drivers).forEach(key => {
-    drivers[key](sinks[key]);
-  });
+  const proxyDOMSource = new Rx.Subject();
+  const sinks = mainFn(proxyDOMSource);
+  const DOMSource = drivers.DOM(sinks.DOM);
+  DOMSource.subscribe(click => proxyDOMSource.onNext(click));
+  // Object.keys(drivers).forEach(key => {
+  //   drivers[key](sinks[key]);
+  // });
 }
 
 const drivers = {
