baa05ae2592efe1da942db5e0802d295729656bb
jcortes <jacortesmahmud@gmail.com>
2016-05-01 18:17:18 -0500

    On branch 07-rxjs-making-our-toy-dom-driver-more-flexible modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -9,21 +9,44 @@ function main (sources) {
       .startWith(undefined)
       .flatMapLatest(() =>
         Rx.Observable.timer(0, 1000)
-        .map((i) => `Seconds elapsed ${i}`)
+        .map((i) => {
+          return {
+            tagName: 'H1',
+            children: [
+              {
+                tagName: 'SPAN',
+                children: [
+                  `Seconds elapsed ${i}`
+                ]
+              }
+            ]
+          };
+        })
       ),
     Log: Rx.Observable.timer(0, 2000)
       .map(i => i * 2)
   }
 }
 
-// source: input (read) effects
-// sink: output (write) effects
-
 // Effects (imperative)
-function DOMDriver (text$) {
-  text$.subscribe(text => {
+function DOMDriver (obj$) {
+  function createElement (obj) {
+    const element = document.createElement(obj.tagName);
+    obj.children
+      .filter(c => typeof c === 'object')
+      .map(createElement)
+      .forEach(c => element.appendChild(c));
+    obj.children
+      .filter(c => typeof c === 'string')
+      .forEach(c => element.innerHTML += c);
+    return element;
+  }
+
+  obj$.subscribe(obj => {
     const container = document.querySelector("#app");
-    container.textContent = text;
+    container.innerHTML = '';
+    const element = createElement(obj);
+    container.appendChild(element);
   });
 
   const DOMSource = Rx.Observable.fromEvent(document, 'click');
@@ -34,32 +57,9 @@ function consoleLogDriver (msg$) {
   msg$.subscribe(msg => console.log(msg));
 }
 
-// bProxy = ...
-// a = f(bProxy)
-// b = g(a)
-// bProxy.imitate(b)
-
-// function run (mainFn, drivers) {
-//   // const proxyDOMSource = new Rx.Subject();
-//   // const sinks = mainFn(proxyDOMSource);
-//   // const DOMSource = drivers.DOM(sinks.DOM);
-//   // DOMSource.subscribe(click => proxyDOMSource.onNext(click));
-//
-//   const proxySources = {};
-//   Object.keys(drivers).forEach(key => {
-//     proxySources[key] = new Rx.Subject();
-//   });
-//   const sinks = mainFn(proxySources);
-//   Object.keys(drivers).forEach(key => {
-//     const source = drivers[key](sinks[key]);
-//     source.subscribe(x => proxySources[key].onNext(x));
-//   });
-// }
-
 const drivers = {
   DOM: DOMDriver,
   Log: consoleLogDriver
 };
 
-// run(main, drivers);
 Cycle.run(main, drivers);
