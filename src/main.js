5989f662413cbac3a1a8e209b2d1cb87786129fe
jcortes <jacortesmahmud@gmail.com>
2016-05-01 19:33:47 -0500

    On branch 10-rxjs-from-toy-dom-driver-to-real-dom-driver modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,30 +1,13 @@
 import {Rx} from 'rx';
 import Cycle from '@cycle/core';
+import CycleDOM from '@cycle/dom';
 
-function h (tagName, children) {
-  return {
-    tagName: tagName,
-    children: children
-  };
-}
+const {h, h1, span, makeDOMDriver} = CycleDOM;
 
-function h1 (children) {
-  return {
-    tagName: 'H1',
-    children: children
-  };
-}
-
-function span (children) {
-  return {
-    tagName: 'SPAN',
-    children: children
-  };
-}
 
 // Logic (functional)
 function main (sources) {
-  const click$ = sources.DOM.selectEvents('span', 'mouseover');
+  const click$ = sources.DOM.select('span').events('mouseover');
   return {
     DOM: click$
       .startWith(undefined)
@@ -44,41 +27,43 @@ function main (sources) {
 }
 
 // Effects (imperative)
-function DOMDriver (obj$) {
-  function createElement (obj) {
-    const element = document.createElement(obj.tagName);
-    obj.children
-      .filter(c => typeof c === 'object')
-      .map(createElement)
-      .forEach(c => element.appendChild(c));
-    obj.children
-      .filter(c => typeof c === 'string')
-      .forEach(c => element.innerHTML += c);
-    return element;
-  }
-
-  obj$.subscribe(obj => {
-    const container = document.querySelector("#app");
-    container.innerHTML = '';
-    const element = createElement(obj);
-    container.appendChild(element);
-  });
-
-  const DOMSource = {
-    selectEvents: function (tagName, eventType) {
-      return Rx.Observable.fromEvent(document, eventType)
-        .filter(ev => ev.target.tagName === tagName.toUpperCase());
-    }
-  };
-  return DOMSource;
-}
+// function makeDOMDriver (mountSelector) {
+//   return function DOMDriver (obj$) {
+//     function createElement (obj) {
+//       const element = document.createElement(obj.tagName);
+//       obj.children
+//         .filter(c => typeof c === 'object')
+//         .map(createElement)
+//         .forEach(c => element.appendChild(c));
+//       obj.children
+//         .filter(c => typeof c === 'string')
+//         .forEach(c => element.innerHTML += c);
+//       return element;
+//     }
+//
+//     obj$.subscribe(obj => {
+//       const container = document.querySelector(mountSelector);
+//       container.innerHTML = '';
+//       const element = createElement(obj);
+//       container.appendChild(element);
+//     });
+//
+//     const DOMSource = {
+//       selectEvents: function (tagName, eventType) {
+//         return Rx.Observable.fromEvent(document, eventType)
+//           .filter(ev => ev.target.tagName === tagName.toUpperCase());
+//       }
+//     };
+//     return DOMSource;
+//   };
+// }
 
 function consoleLogDriver (msg$) {
   msg$.subscribe(msg => console.log(msg));
 }
 
 const drivers = {
-  DOM: DOMDriver,
+  DOM: makeDOMDriver('#app'),
   Log: consoleLogDriver
 };
 
