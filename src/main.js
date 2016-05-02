950208bbd9a8a6989a800c9e3c2e2be6fb9261eb
jcortes <jacortesmahmud@gmail.com>
2016-05-01 19:48:21 -0500

    On branch 11-rxjs-hello-world-in-cycle-js modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -2,69 +2,26 @@ import {Rx} from 'rx';
 import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
 
-const {h, h1, span, makeDOMDriver} = CycleDOM;
-
+const {input, label, hr, div, h1, makeDOMDriver} = CycleDOM;
 
 // Logic (functional)
 function main (sources) {
-  const click$ = sources.DOM.select('span').events('mouseover');
+  const inputEv$ = sources.DOM.select('.field').events('input');
+  const name$ = inputEv$.map(ev => ev.target.value).startWith('');
   return {
-    DOM: click$
-      .startWith(undefined)
-      .flatMapLatest(() =>
-        Rx.Observable.timer(0, 1000)
-        .map(i =>
-          h1({style: {background: 'red'}}, [
-            span([
-              `Seconds elapsed ${i}`
-            ])
-          ])
-        )
-      ),
-    Log: Rx.Observable.timer(0, 2000)
-      .map(i => i * 2)
+    DOM: name$.map(name =>
+      div([
+        label('Name: '),
+        input('.field', {type: 'text'}),
+        hr(),
+        h1('', `Hola ${name}!`)
+      ])
+    )
   }
 }
 
-// Effects (imperative)
-// function makeDOMDriver (mountSelector) {
-//   return function DOMDriver (obj$) {
-//     function createElement (obj) {
-//       const element = document.createElement(obj.tagName);
-//       obj.children
-//         .filter(c => typeof c === 'object')
-//         .map(createElement)
-//         .forEach(c => element.appendChild(c));
-//       obj.children
-//         .filter(c => typeof c === 'string')
-//         .forEach(c => element.innerHTML += c);
-//       return element;
-//     }
-//
-//     obj$.subscribe(obj => {
-//       const container = document.querySelector(mountSelector);
-//       container.innerHTML = '';
-//       const element = createElement(obj);
-//       container.appendChild(element);
-//     });
-//
-//     const DOMSource = {
-//       selectEvents: function (tagName, eventType) {
-//         return Rx.Observable.fromEvent(document, eventType)
-//           .filter(ev => ev.target.tagName === tagName.toUpperCase());
-//       }
-//     };
-//     return DOMSource;
-//   };
-// }
-
-function consoleLogDriver (msg$) {
-  msg$.subscribe(msg => console.log(msg));
-}
-
 const drivers = {
-  DOM: makeDOMDriver('#app'),
-  Log: consoleLogDriver
+  DOM: makeDOMDriver('#app')
 };
 
 Cycle.run(main, drivers);
